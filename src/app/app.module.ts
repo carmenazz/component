415d4936749abddac715fb8bd3051ce53ac899f0
carmenazz <carmen.zorrilla@newhorizons.edu.do>
2021-11-16 17:51:21 +0000

    finish project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageComponent } from './image/image.component';
+import { TableComponent } from './table/table.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageComponent,
+    TableComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
